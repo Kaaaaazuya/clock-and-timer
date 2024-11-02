250d3899b6cfcfa2472187c343c0be8243589c45
Kazuya Yokoyama <yk3kzy@gmail.com>
2024-11-02 16:10:29 +0900

    ログイン情報を db で管理

--- auth.ts
+++ auth.ts
@@ -1,8 +1,11 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { prisma } from "~/app/lib/prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
 	trustHost: true, // docker環境で利用するため
 	debug: process.env.NODE_ENV !== "production",
 	providers: [Google],
+	adapter: PrismaAdapter(prisma),
 });
