069c0206ba7bba947eca45df4cd5dadfffeb4008
Kazuya Yokoyama <yk3kzy@gmail.com>
2024-11-02 17:35:48 +0900

    vitest の導入

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath } from "url";
+import { configDefaults, defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+	plugins: [react()],
+	test: {
+		globals: true,
+		exclude: [...configDefaults.exclude, "**/e2e/**"],
+		alias: {
+			"~/": fileURLToPath(new URL("./src/", import.meta.url)),
+		},
+		environment: "happy-dom",
+		coverage: {
+			provider: "v8",
+			reporter: ["text", "html"],
+			reportsDirectory: "./coverage",
+		},
+	},
+});
